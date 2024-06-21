c30877b274d2439a60a8fafd97893ede0a8f0b67
shauryaa19 <120932081+shauryaa19@users.noreply.github.com>
2024-06-22 02:42:31 +0530

    implements cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const jsonServer = require("json-server"); // importing json-server library
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
